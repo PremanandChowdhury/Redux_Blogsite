9327068aa3b98f6312cd91ffc3d624132641e41e
PremanandChowdhury <freudigprem@gmail.com>
2022-01-23 00:07:19 +0530

    add: Connecting redux to the Component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
-export default class PostList extends Component {
+class PostList extends Component {
+  
+  // Whenever the Component renders the action is called
+  componentDidMount() {
+    this.props.fetchPosts()
+  }
+
   render() {
     return <div>Post List</div>
   }
 }
+
+export default connect(null, { fetchPosts })(PostList)
