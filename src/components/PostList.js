c06cc6fe7b4db5f448b73e5c931f15ddfe79b018
PremanandChowdhury <freudigprem@gmail.com>
2022-02-23 23:22:46 +0530

    code: Post Listing component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,15 +3,35 @@ import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
 class PostList extends Component {
-  
   // Whenever the Component renders the action is called
   componentDidMount() {
     this.props.fetchPosts()
   }
 
+  renderList() {
+    return this.props.posts.map((post) => {
+      return (
+        <div className='item' key={post.id}>
+          <i className='large middle aligned icon user'></i>
+          <div className='content'>
+            <div className='description'>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
-    return <div>Post List</div>
+    return <div className='ui relaxeed divided list'>{this.renderList()}</div>
   }
 }
 
-export default connect(null, { fetchPosts })(PostList)
+// Selecting part of data from the store
+const mapStateToProps = (state) => {
+  return { posts: state.posts }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList)
